2b57750b841fa51d5ac45e22b540d30dcbe95f50
HariprasadNuttu <hariprasadnuttu@gmail.com>
2018-11-13 22:23:36 +0530

    Hariprasad-changes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 
 // Dependencies
 var server = require('./lib/server');
-var workers = require('./lib/workers');
 
 // Declare the app
 var app = {};
@@ -16,8 +15,6 @@ app.init = function(){
   // Start the server
   server.init();
 
-  // Start the workers
-  workers.init();
 
 };
 
--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,34 @@
-const options = {
-    client: 'mysql',
-    connection: {
-      user: 'root',
-      password: 'p@ssw0rd',
-      database: 'Vdart_Technologies'
-    }
-}
-
+// const options = {
+//     client: 'mysql',
+//     connection: {
+//       user: 'root',
+//       password: 'p@ssw0rd',
+//       database: 'Vdart_Technologies'
+//     }
+// }
+var config = require('./lib/config');
+console.log(config)
 // const options = {
 //     client: 'pg',
 //     connection:'postgres://localhost/sammy'
 // }
 
-const knex = require('knex')(options);
-
+const knex = require('knex')(config.dbSetup);
+//
 knex.schema.createTable('message_list', (table) => {
-    table.increments('id').primary()
-    table.string('message_contents').nullable()
+    table.uuid('id')
+    table.text('message_contents',['longtext']).nullable()
     table.timestamp('received_ts').nullable()
 }).then(() => console.log("table created"))
     .catch((err) => { console.log(err); throw err })
     .finally(() => {
-        knex.destroy();
+        // knex.destroy();
     });
+
+    // knex('message_list').insert([{id:"8a0b60ca-e74f-11e8-9027-9c2a70c315ed",received_ts: new Date()}]).then(() => console.log("data inserted"))
+    // .catch((err) => { console.log("==================FAilure================"); console.log(err); throw err })
+    // .finally(() => {
+    //     // knex.destroy();
+    // });
 
     module.exports = knex;
